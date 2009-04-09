5fb6ab5187b8f72fbe0cfcc6a50c649b6a08e5fd
Gilles Mathurin <gillesmath@mac.com>
2009-04-09 18:38:44 -0400

    creation des ressources House, Location, Customer, Category et Tarif

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :tarifs
+
+  map.resources :categories
+
+  map.resources :customers
+
+  map.resources :locations
+
+  map.resources :houses
+
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
